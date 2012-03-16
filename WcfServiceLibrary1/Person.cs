4b1fa62ba506ea833c002a3c93e689de09a481a6
Alexander Perry <yrrepxela@gmail.com>
2012-03-16 15:47:42 +0100

    added person object and arraylist, not working atm

---
+++ WcfServiceLibrary1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+    }
+}
+
